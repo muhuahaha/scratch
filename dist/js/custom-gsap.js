488a5d35ed949e55c88e740341e6c167925d5c5c
muhuahaha <kimyou2019@gmail.com>
2020-06-06 00:10:11 +0200

    wahnn

--- dist/js/custom-gsap.js
+++ dist/js/custom-gsap.js
@@ -45,7 +45,7 @@ tl.from('.purple p', { scale: 0.3, rotation: 45, autoAlpha: 0, ease: 'power2' })
 gsap.utils.toArray('.nav-link a').forEach(function(a) {
   a.addEventListener('click', function(e) {
     e.preventDefault();
-    gsap.to(window, { duration: 1, scrollTo: e.target.getAttribute('href') });
+    gsap.to(window, { duration: 1, autoKill: false, scrollTo: e.target.getAttribute('href') });
   });
 });
 

9a8edec04d51df9c772b7206d200208ccf8ac6fe
muhuahaha <kimyou2019@gmail.com>
2020-06-05 22:32:41 +0200

    hide menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,45 +1,37 @@
 const header = document.querySelector('.main-header');
 
 window.addEventListener('scroll', () => {
-  let scrollPos = window.scrollY;
-  const currentScrollPos = window.scrollY;
+  const scrollPos = window.scrollY;
   if (scrollPos > 10) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
   }
-  if (scrollPos > currentScrollPos) {
-    document.getElementById('testtest').style.top = '0';
-  } else {
-    document.getElementById('testtest').style.top = '-89px';
-  }
-  scrollPos = currentScrollPos;
 });
 
-// /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-// const prevScrollpos = window.scrollY;
-// window.onscroll = function() {
-//   const currentScrollPos = window.scrollY;
-//   if (scrollPos > currentScrollPos) {
-//     document.getElementById('testtest').style.top = '0';
-//   } else {
-//     document.getElementById('testtest').style.top = '-89px';
-//   }
-//   scrollPos = currentScrollPos;
-// };
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('testtest').style.top = '0';
+  } else {
+    document.getElementById('testtest').style.top = '-50px';
+  }
+  prevScrollpos = currentScrollPos;
+};
 
-const headerTest = document.getElementById('myDIV');
-console.log(headerTest);
-const btns = headerTest.getElementsByClassName('nav-link');
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', function() {
-    const current = document.getElementsByClassName('active');
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(' active', '');
-    }
-    this.className += ' active';
-  });
-}
+// const headerTest = document.getElementById('myDIV');
+// console.log(headerTest);
+// const btns = headerTest.getElementsByClassName('nav-link');
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener('click', function() {
+//     const current = document.getElementsByClassName('active');
+//     if (current.length > 0) {
+//       current[0].className = current[0].className.replace(' active', '');
+//     }
+//     this.className += ' active';
+//   });
+// }
 
 // const check = document.getElementById('menu-btn').checked === true ? alert('ASDAS') : alert('ZZZZ');
 

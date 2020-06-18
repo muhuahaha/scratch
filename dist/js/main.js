88087dd4496630f062fe72ba34615668ef38e70a
muhuahaha <kimyou2019@gmail.com>
2020-06-18 03:31:39 +0200

    testing

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.main-header');
 
 window.addEventListener('scroll', () => {
   const scrollPos = window.scrollY;
-  if (scrollPos > 800) {
+  if (scrollPos > 100) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
--- dist/js/type.js
+++
@@ -1,5 +0,0 @@
-// if (document.getElementById('marquee')) {
-//   const laufschrift = document.getElementById('marquee');
-//   const len = laufschrift.innerHTML.length;
-//   laufschrift.firstChild.style.animationDuration = `${len / 20}s`;
-// }

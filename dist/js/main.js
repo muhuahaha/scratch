af82a8fb72e0195c44162f2e4619d3905ced8faa
muhuahaha <kimyou2019@gmail.com>
2020-06-19 23:46:37 +0200

    testing

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,7 +21,7 @@ window.addEventListener('scroll', () => {
 // };
 
 const headerTest = document.getElementById('myDIV');
-console.log(headerTest);
+// console.log(headerTest);
 const btns = headerTest.getElementsByClassName('nav-link');
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', function() {
@@ -42,7 +42,44 @@ function myFunction() {
 }
 
 document.getElementById('logo-btn').addEventListener('click', () => {
-  console.log('click');
+  // console.log('click');
   document.getElementById('menu-btn').checked = false;
 });
 document.getElementById('myDIV').addEventListener('click', myFunction);
+
+function startTime() {
+  const today = new Date();
+  const h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML = `${h}<br />${m}<br />${s}`;
+  const t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = `0${i}`;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
+document.getElementById('clock').addEventListener('load', startTime());
+
+const preloader = document.querySelector('.preloader');
+
+const fadeEffect = setInterval(() => {
+  // if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
+  if (!preloader.style.opacity) {
+    preloader.style.opacity = 1;
+    const element = document.querySelector('.preloader');
+    element.classList.add('loaded');
+  }
+  if (preloader.style.opacity > 0) {
+    preloader.style.opacity -= 0.1;
+  } else {
+    clearInterval(fadeEffect);
+  }
+}, 200);
+
+window.addEventListener('load', fadeEffect);

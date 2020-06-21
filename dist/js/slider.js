775ec4603c2e9b1956da93a62502545da38a517a
muhuahaha <kimyou2019@gmail.com>
2020-06-22 00:01:01 +0200

    testing

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,11 +1,14 @@
-new Glider(document.querySelector('.glider'), {
-  slidesToScroll: 1,
-  slidesToShow: 1,
-  draggable: true,
-  dots: '.dots',
-  rewind: true,
-  arrows: {
-    prev: '.glider-prev',
-    next: '.glider-next',
-  },
+window.addEventListener('load', function() {
+  new Glider(document.querySelector('.glider'), {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    draggable: true,
+    dots: '.dots',
+    dragVelocity: 3.8,
+    rewind: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next',
+    },
+  });
 });

c655b65a8db3995f8648d73fd5184b4fc8b4d763
muhuahaha <kimyou2019@gmail.com>
2020-06-22 19:09:13 +0200

    testing

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', function() {
   new Glider(document.querySelector('.glider'), {
-
     slidesToScroll: 1,
     slidesToShow: 1,
     draggable: true,
     dots: '.dots',
-   rewind: true,
-    dragVelocity: 3.8,
-    scrollLockDelay: 350,
-    scrollLock: true,
+    rewind: true,
+
+    // dragVelocity: 3.8,
+    scrollLockDelay: 250,
+    scrollLock: false,
     propagateEvent: true,
     arrows: {
       prev: '.glider-prev',

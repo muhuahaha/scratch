361ab66a535c8a3946e2c3ce965ed2af3c469c04
muhuahaha <47262721+muhuahaha@users.noreply.github.com>
2020-06-22 08:07:17 +0200

    Kk

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,8 +1,17 @@
-var glide = new Glide('.glider', {
-  type: 'carousel',
-  perView: 1,
-  focusAt: 'center',
-  }
-})
-
-glide.mount()
+window.addEventListener('load', function() {
+  new Glider(document.querySelector('.glider'), 
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    draggable: true,
+    dots: '.dots',
+   rewind: true,
+    dragVelocity: 3.8,
+    scrollLockDelay: 350,
+    scrollLock: true,
+    propagateEvent: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next',
+    },
+  });
+});

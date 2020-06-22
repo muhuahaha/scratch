9889c03f8a29619878d49b76a0b581f5de5f80ab
muhuahaha <47262721+muhuahaha@users.noreply.github.com>
2020-06-22 07:28:30 +0200

    Kk

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -2,11 +2,12 @@
 
 window.addEventListener('load', function() {
   new Glider(document.querySelector('.glider'), {
-    slidesToScroll: 1,
-    slidesToShow: 1,
+    type: 'carousel',
+  startAt: 0,
+  perView: 3,
     draggable: true,
     dots: '.dots',
-    rewind: true,
+ 
     dragVelocity: 3.8,
     scrollLockDelay: 350,
     scrollLock: true,
